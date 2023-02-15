ca8f2cfdc877ea77ce03dbcf75818c0eabac12b3
Nabil Islam <nabil8976@gmail.com>
2023-02-15 22:09:33 +1100

    initial additions for to-do list app

--- hello/views.py
+++ hello/views.py
@@ -9,4 +9,6 @@ def brian(request):
     return HttpResponse("Hello, brian!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize(),
+    })
